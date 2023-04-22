8c6fa89d5ee1879eb5b985050ee2e5d6b15153c1
Deep Manek <dpmanek@gmail.com>
2023-04-22 13:33:59 -0400

    adding route

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const constructorMethod = (app) => {
 	app.use('/users', usersRoutes); // Login/Signup (API)Routes
 	app.use('/user/data', userDataRoutes); // verify user and get specific data  Uses Middle Ware to Verify
 	app.use('/public', publicRoutes); // No Middle Ware route should display all public Data
-
+	app.get('/', (req, res) => {
+		res.send('hello');
+	});
 	app.use('*', (req, res) => {
 		res.status(404).json({ error: 'Not Found' });
 	});
