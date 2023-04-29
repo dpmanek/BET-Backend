0b876598b09103369fe62d544ff0b5bfdc8ae35b
Deep Manek <dpmanek@gmail.com>
2023-04-29 08:58:13 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const constructorMethod = (app) => {
 	app.use('/user/data', userDataRoutes); // verify user and get specific data  Uses Middle Ware to Verify
 	app.use('/public', publicRoutes); // No Middle Ware route should display all public Data
 	app.get('/', (req, res) => {
-		res.send('hello');
+		res.status(200).send('Hello server is running').end();
 	});
 	app.use('*', (req, res) => {
 		res.status(404).json({ error: 'Not Found' });
